9eea1843c9c616f8137c4212da5c465e3ba640bc
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-08-07 14:25:27 +0300

    Add project files.

---
+++ GovernmentTaskManagement.backend/Data/toolDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+namespace GovernmentTaskManagement.backend.Data
+{
+    public class toolDbContext(DbContextOptions<toolDbContext> options) : IdentityDbContext<IdentityUser>(options) // identity enables us to use ASPNET USER AND ROLES tables for auth and authorization
+    {
+        
+    }
+}
