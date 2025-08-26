76f0e4f06bd46d1e685abcd658e513bd563c4375
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-08-26 15:26:11 +0300

    controller working

---
+++ GovTaskManagement.Infrastructure/InfrastructureDI.cs
@@ -0,0 +1,63 @@
+﻿using GovTaskManagement.Application.Interfaces.Repositories;
+using GovTaskManagement.Application.Interfaces.ServiceInterfaces;
+using GovTaskManagement.Application.Services;
+using GovTaskManagement.Infrastructure.Data;
+using GovTaskManagement.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace GovTaskManagement.Infrastructure
+{
+    public static class InfrastructureDI
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection Services, IConfiguration Configuration)
+        {
+            Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            Services.AddScoped<IUserRepository, UserRepository>();
+            Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            Services.AddScoped<IDocumentRepository, DocumentRepository>();
+            Services.AddScoped<ITaskRepository, TaskRepository>();
+
+            Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            Services.AddScoped<IAuthService, AuthService>();
+            Services.AddScoped<ITaskService, TaskService>();
+            Services.AddScoped<IDepartmentService, DepartmentService>();
+            Services.AddScoped<IDocumentService, DocumentService>();
+
+
+            Services.AddDbContext<ToolDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            var jwtKey = Configuration["Jwt:Key"];
+            var jwtIssuer = Configuration["Jwt:Issuer"];
+
+            Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtIssuer,
+                    ValidAudience = jwtIssuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+                };
+            });
+
+            return Services;
+        }
+    }
+}
--- GovernmentTaskManagement.backend/Program.cs
+++ GovernmentTaskManagement.backend/Program.cs
@@ -1,87 +1,39 @@
-using GovTaskManagement.Application.Interfaces.Repositories;
-using GovTaskManagement.Application.Interfaces.ServiceInterfaces;
-using GovTaskManagement.Application.Services;
 using GovTaskManagement.Domain.Entities;
+using GovTaskManagement.Infrastructure;
 using GovTaskManagement.Infrastructure.Data;
-using GovTaskManagement.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-          var jwtKey = builder.Configuration["Jwt:Key"];
-          var jwtIssuer = builder.Configuration["Jwt:Issuer"];
+builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddAuthentication(options =>
+builder.Services.AddIdentity<ApiUser, IdentityRole>()
+.AddEntityFrameworkStores<ToolDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = jwtIssuer,
-        ValidAudience = jwtIssuer,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
-    };
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-    // Add services to the container.
-    builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-    builder.Services.AddScoped<IUserRepository, UserRepository>();
-    builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
-    builder.Services.AddScoped<IDocumentRepository, DocumentRepository>();
-    builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+app.UseHttpsRedirection();
 
-    builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+app.UseAuthentication();
+app.UseAuthorization();
 
-    builder.Services.AddScoped<IAuthService, AuthService>();
-    builder.Services.AddScoped<ITaskService, TaskService>();
-    builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+app.MapControllers();
 
-
-    builder.Services.AddIdentity<ApiUser, IdentityRole>()
-    .AddEntityFrameworkStores<ToolDbContext>()
-    .AddDefaultTokenProviders();
-
-    builder.Services.AddDbContext<ToolDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-
-    var app = builder.Build();
-
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthentication();
-    app.UseAuthorization();
-
-
-    app.MapControllers();
-
-    app.Run();
-});
-
-
-
+app.Run();
 
 
