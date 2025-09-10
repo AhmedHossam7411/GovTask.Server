f9656ab011e22d6ef545974f654925ca2c89a679
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-09-10 17:04:16 +0300

    remove unused imports

--- GovernmentTaskManagement.backend/Controllers/DocumentController.cs
+++ GovernmentTaskManagement.backend/Controllers/DocumentController.cs
@@ -5,11 +5,7 @@ using GovTaskManagement.Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace GovernmentTaskManagement.Api.Controllers
 {
