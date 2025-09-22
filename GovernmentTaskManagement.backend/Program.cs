aebe70cd00374eba8966bc1bf9b6831cdce94b45
AhmedHossam7411 <ahmedabdelkhalek.7411@gmail.com>
2025-09-22 11:16:11 +0300

    cors

--- GovernmentTaskManagement.backend/Program.cs
+++ GovernmentTaskManagement.backend/Program.cs
@@ -19,6 +19,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy => policy.WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
